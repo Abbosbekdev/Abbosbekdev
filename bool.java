4c51751be4f771fdd3595e1e855299998ef0ca76
Abbosbek Ustaboyev Botirjonovich <91521657+Abbosbekdev@users.noreply.github.com>
2021-10-02 10:17:08 +0500

    Create bool.java
    bool masalalar yechimi javada

---
+++ bool.java
@@ -0,0 +1,138 @@
+package com.company;
+
+import org.ietf.jgss.GSSContext;
+
+import java.util.Scanner;
+
+public class bool {
+
+    public static void main(String[] args) {
+     //1-2-3-masasla
+        Scanner scanf = new Scanner(System.in);
+        /*System.out.print("a = ");
+        int a=scanf.nextInt();
+        System.out.print("b = ");
+        int b= scanf.nextInt();
+        System.out.print("c = ");
+        int c= scanf.nextInt();
+        boolean son;
+        /*boolean juft,toq;
+        toq = a>0 && a%2 == 1;
+        juft = a%2==0;
+        System.out.print(juft);
+        System.out.print(toq);*/
+    //4-5-masala
+        /*boolean son;
+        son = a > 0 && b<=3 || a>=0 && b<-2;
+        System.out.print("son = " + son);*/
+     //6-7-masala
+        /*son = a <= b && b <= c;
+        System.out.print(son);*/
+    //8-masala
+        /*son = a%2==1 && b%2==1;
+        System.out.print(son);*/
+    //9-masala
+        /*son = a%2==1 || b%2==1;
+        System.out.print(son);*/
+    //10-masala
+        /*
+        son = a%2==0 && b%2==1 || a%2==1 && b%2==0;
+        System.out.print(son);*/
+    //11-masala
+        /*
+        son = a%2==1 && b%2==1 || a%2==0 && b%2==0;
+        System.out.print(son);
+     //12-masala
+        son = a%2==1 && b%2==1 && c%2==1;
+        System.out.print(son);
+    //13-masala
+        son = a>0 || b>0 || c>0;
+        System.out.print(son);
+     //14-masala
+        son = a>0 && b<0 && c<0 || a<0 && b>0 && c<0 || a<0 && b<0 && c>0;
+        System.out.print(son);
+     //15-masala
+        son = a>0 && b>0 && c<0 || a<0 && b>0 && c>0 || a>0 && b<0 && c>0;
+        System.out.print(son);
+     //16-masala
+        son = a / 10 > 0 && a /100 == 0 && a%2==0;
+        System.out.println(son);
+     //17-masala
+        son = b/100>0 && b%2==1 && b/1000==0;
+        System.out.println(son);
+     //18-masala
+        son = a==b || a==c||c==b;
+        System.out.println(son);
+     //19-masala
+        son = a+b==0 || a+c==0 || b+c==0;
+        System.out.println(son);
+     //20-masala
+        son = a/100 != a%100/10 && a%100/10 != a%10 && a/100 != a%10;
+        System.out.println(son);
+     //21-masala
+        son =b%10-b%100/10==1 && b%100/10-b/100==1;
+        System.out.println(son);
+     //22-masala
+        son = c%10-c%100/10==1 && c%100/10-c/100==1 || c%100/10-c%10==1 && c/100-c%100/10==1;
+        System.out.println(son);
+     //23-masala
+        son = a/100==a%10 && a/100 !=a%100/10;
+        System.out.println(son);
+     //24-masala
+        son = b*b-4*a*c>=0;
+        System.out.println(son);
+     //25-masala*/
+        System.out.print("x = ");
+        double x=scanf.nextDouble();
+        System.out.print("y = ");
+        double y=scanf.nextDouble();
+        System.out.print("x1 = ");
+        int x1=scanf.nextInt();
+        System.out.print("y1 = ");
+        int y1=scanf.nextInt();
+        boolean son;/*
+        son = x<0 && y>0;
+        System.out.println(son);
+     //26-masala
+        son = x>0 && y<0;
+        System.out.println(son);
+     //27-masala
+        son = x>0 && y<0 || x>0 && y<-;
+        System.out.println(son);
+     //28-masala
+        son = x>0 && y>0 || x<0 && y<0;
+        System.out.println(son);
+     //30-masala
+        son = a==b && b==c ;
+        System.out.println(son);
+     //31-masala
+        son = a==b || b==c || a==c;
+        System.out.println(son);
+     //32-masala
+        son = a*a+b*b==c*c;
+        System.out.println(son);
+     //33-masala
+        son = a+b>c && b+c>a && a+c>b;
+        System.out.println(son);
+     //34-masala
+        son =(x+y)%2==1;
+        System.out.println(son);
+     //35-masala
+        son =(x+y)%2==(x1+y1)%2;
+        System.out.print(son);
+     //36-masala
+        son = x==x1 || y==y1;
+        System.out.println(son);
+     //37-masala
+        son = x1==x+1 || x1==x-1 && y1==y+1 || y1==y-1;
+        System.out.println(son);
+     //38-masala
+        son = x1>x || y1>y && x+y==x1+y1 || x==y && x1==y1 || x+y1==x1+y;
+        System.out.println(son);
+     //39-masala
+        son =x1>x || y1>y && x+y==x1+y1 || y1==y && x1==x || x+y1==x1+y && x1==x+1 || x1==x-1 && y1==y+1 || y1==y-1;
+        System.out.println(son);*/
+     //40-masala
+           
+    }
+}
